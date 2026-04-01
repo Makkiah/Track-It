36ceb45d31f535d13e2d0b4ff914516cedfae298
makkiah <makkiah.muhammad@powerdistributors.com>
2026-04-01 10:15:51 -0400

    A1-4 (Docs/Pancake)

--- TrackIt/components/tracker/trackerData.tsx
+++ TrackIt/components/tracker/trackerData.tsx
@@ -2,10 +2,10 @@
 export const habits = {
     Reading : ["Read for 5 minutes", "Read for 10 minutes", "Read for 15 minutes (03/29/26)"],
     Coding: ["Coded for 6 minutes", "Coded for 1 hour", "Coded for 30 minutes", "Coded for 10 minutes (03/29/26)", "Day 2 of learning / 60 Minutes of Coding"], 
-    Music: ["Headlined UAC event (3/28)", "Visited the Dallas (3/9, 3/30, 3/31)", "Visted Social Club / SkyboxxxLounge open mic Monday", "Found out about 'Kafe Kerouac' tuesday open mic"],
+    Music: ["Attended a musician contest at UAC in Feb", "Headlined UAC event (3/28)", "Visited the Dallas (3/9, 3/30, 3/31)", "Visted Social Club / SkyboxxxLounge open mic Monday", "Found out about 'Kafe Kerouac' tuesday open mic"],
     Gym: [],
     Japanese: [],
-    Life: ["Got two pair of new glasses", "Got some eye contacts", "Started an AI Coding Bootcamp", "Finished Blood Meridian"],
+    Life: ["Got two pair of new glasses", "Got some eye contacts", "Started an AI Coding Bootcamp", "Finished Blood Meridian", "Got some Doc Martin boots", "Started a relationship with Pancake (my best friend)"],
     "To-Do": [ "Day 1 of homework", "Day 2 of homework", "Day 3 Learning", "Seperate the 'To-Do' area and create a seperate area for 'What you've learned today'", "Finish book 'Melanin: The Chemical To Black Greatness'", "Add this project to Makkiah.com", "Add 'yearly goals' area at the top that never changes" ]
 }
 
