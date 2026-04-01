60e31a99d75bb7f739401cd45a9b1c080be8d8b1
Makkiah Muhammad <makkiahf@gmail.com>
2026-04-01 09:47:46 -0400

    April1st

--- TrackIt/components/tracker/trackerData.tsx
+++ TrackIt/components/tracker/trackerData.tsx
@@ -1,11 +1,11 @@
 
 export const habits = {
     Reading : ["Read for 5 minutes", "Read for 10 minutes", "Read for 15 minutes (03/29/26)"],
-    Coding: ["Coded for 6 minutes", "Coded for 1 hour", "Coded for 30 minutes", "Coded for 10 minutes (03/29/26)"],
+    Coding: ["Coded for 6 minutes", "Coded for 1 hour", "Coded for 30 minutes", "Coded for 10 minutes (03/29/26)", "Day 2 of learning / 60 Minutes of Coding"], 
     Music: [],
     Gym: [],
     Japanese: [],
-    "To-Do": [ "Day 1 of homework", "Day 2 of learning", "Seperate the 'To-Do' area and create a seperate area for 'What you've learned today'" ]
+    "To-Do": [ "Day 1 of homework", "Day 2 of homework", "Seperate the 'To-Do' area and create a seperate area for 'What you've learned today'", "Finish book 'Melanin: The Chemical To Black Greatness'" ]
 }
 
 export const april26habits = {
