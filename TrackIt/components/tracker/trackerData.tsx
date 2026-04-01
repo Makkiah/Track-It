ed1adc7ef174ba5344507403ea98806576d87e16
makkiah <makkiah.muhammad@powerdistributors.com>
2026-04-01 13:10:22 -0400

    A1-8 (update)

--- TrackIt/components/tracker/trackerData.tsx
+++ TrackIt/components/tracker/trackerData.tsx
@@ -1,6 +1,6 @@
 
 export const habits = {
-    Reading : ["Read for 5 minutes", "Read for 10 minutes", "Read for 15 minutes (03/29/26)"],
+    Reading : ["Read for 5 minutes", "Read for 10 minutes", "Read for 15 minutes (03/29/26)", "Read for 6 minutes (4/1) x 2", "Read coding docs (4/1)"],
     Coding: ["Coded for 6 minutes", "Coded for 1 hour", "Coded for 30 minutes", "Coded for 10 minutes (03/29/26)", "Day 2 of learning / 60 Minutes of Coding"], 
     Music: ["Attended a musician contest at UAC in Feb", "Headlined UAC event (3/28)", "Visited the Dallas (3/9, 3/30, 3/31)", "Visted Social Club / SkyboxxxLounge open mic Monday", "Found out about 'Kafe Kerouac' tuesday open mic"],
     Gym: [],
